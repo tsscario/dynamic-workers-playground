b20b09b2615ecfaaba4979e860589d2864bf1123
Tarciso Scariot <tarciso.scariot@rdstation.com>
2026-06-25 15:13:33 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -170,7 +170,7 @@ export default {
       try {
 
         const authHeader = request.headers.get("Authorization");
-        if (!authHeader || authHeader !== `Bearer ${env.API_KEY}`) return new Response(`Unauthorized`, { status: 401 });
+        if (!authHeader || authHeader !== `Bearer ${env.API_KEY}`) return new Response(`Unauthorized.`, { status: 401 });
 
 
         const { code } = (await request.json()) as RunPythonRequestBody;
